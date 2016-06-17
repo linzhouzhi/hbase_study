cce26d047b05b1c38b46dca02f2669d8a05466b8
linzhouzhi <490664646@qq.com>
2016-06-17 03:28:11 -0700

    add follower

--- src/main/java/com/mycompany/app/weibo/dao/FollowsDAO.java
+++ src/main/java/com/mycompany/app/weibo/dao/FollowsDAO.java
@@ -1,7 +1,47 @@
 package com.mycompany.app.weibo.dao;
 
+import com.mycompany.app.service.HbaseHelper;
+import com.mycompany.app.weibo.model.Follows;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.RegexStringComparator;
+import org.apache.hadoop.hbase.filter.RowFilter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by lzz on 6/17/16.
  */
 public class FollowsDAO {
+    private static final String TABLE_NAME = "follows";
+    private static final String COLUMN_FAMILY = "f";
+    private static final HbaseHelper HBase = new HbaseHelper( TABLE_NAME );
+
+    public void addFollow( Follows f ){
+        Map map = new HashMap();
+        map.put( f.getFollowed(),f.getFollowed_name() );
+        String key = keyRow( f.getFollower() , f.getFollowed() );
+        HBase.put( key, COLUMN_FAMILY, map );
+    }
+
+    public String getFollow(String follower, String followed ){
+        String key = keyRow( follower, followed );
+        Result r =  HBase.get( key );
+        byte[] value = r.value();
+        String s_value = new String( value );
+        return s_value;
+    }
+
+    public List<String> getFollowList( String follower ){
+        Filter filter = new RowFilter(CompareFilter.CompareOp.EQUAL,new RegexStringComparator( "^"+follower ) );
+        List<String> list = HBase.getList( COLUMN_FAMILY ,  filter );
+        return list;
+    }
+
+    private String keyRow( String follower, String followed ){
+        return follower + "_" + followed;
+    }
 }
--- src/test/java/com/mycompany/app/weibo/dao/FollowsDAOTest.java
+++ src/test/java/com/mycompany/app/weibo/dao/FollowsDAOTest.java
@@ -1,7 +1,21 @@
 package com.mycompany.app.weibo.dao;
 
+import com.mycompany.app.weibo.model.Follows;
+import org.junit.Test;
+
 /**
  * Created by lzz on 6/17/16.
  */
 public class FollowsDAOTest {
+    private static FollowsDAO fDao = new FollowsDAO();
+    @Test
+    public void testAddFollowe(){
+        Follows follows = new Follows( "lzz", "ldd", "helloldd");
+        fDao.addFollow( follows );
+    }
+
+    @Test
+    public void testGetFollowe(){
+        fDao.getFollow( "lzz", "ldd" );
+    }
 }
